bd2f829366f92cddd2de356594c8022d02379bbc
Dylan Connolly <dylanconnolly145@gmail.com>
2021-02-22 18:00:41 -0700

    Create spec for sending a new message between users

---
+++ spec/requests/new_message_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe 'when a POST request is made with a request body containing existing users and a message' do
+    it "a new message should be created and added to the conversation between the two users" do
+        user1 = User.create(name: "User1")
+        user2 = User.create(name: "User2")
+        
+        request = {
+            sender: "User1",
+            recipient: "User2",
+            content: "Hi there, User2"
+        }
+
+        post '/api/v1/messages', params: request
+
+        expect(response).to be_successful
+    end
+end
